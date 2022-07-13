079caa50cbfd92df7b3731fea453e0181820cbfb
Diogo Vieira <diogovieira341di@gmail.com>
2022-07-13 19:05:53 -0300

    sla

---
+++ js/links.js
@@ -0,0 +1,24 @@
+const menuItems =document.querySelectorAll('.navegação .padd');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 200;
+    scrollToPosition(to)
+
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
